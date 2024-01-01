6ee853dbe3d0a1ad3343045341c9abee6b2cf0d5
niiion <niion.eooo@gmail.com>
2024-01-02 07:28:32 +0900

    [initial] 10998. 곱셈 문제

---
+++ baekjoon/step01/_10998.java
@@ -0,0 +1,15 @@
+package baekjoon.stage01;
+
+import java.util.Scanner;
+
+public class _10998 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a * b);
+    }
+}
