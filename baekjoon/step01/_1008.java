c51d01a1a1daa305697d2bc857d4fa59ae7665ec
niiion <niion.eooo@gmail.com>
2024-01-02 07:19:04 +0900

    [initial] 1008. 나눗셈 문제

---
+++ baekjoon/step01/_1008.java
@@ -0,0 +1,15 @@
+package baekjoon.stage01;
+
+import java.util.Scanner;
+
+public class _1008 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextInt();
+        double b = scanner.nextInt();
+
+        System.out.println(a / b);
+    }
+}
