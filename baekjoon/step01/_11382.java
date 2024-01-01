2aa941e8ef62c01cf738abfb8b4fe0b1ce518c2d
niiion <niion.eooo@gmail.com>
2024-01-02 07:29:08 +0900

    [initial] 11382. 더 큰 수를 더하는 문제

---
+++ baekjoon/step01/_11382.java
@@ -0,0 +1,16 @@
+package baekjoon.stage01;
+
+import java.util.Scanner;
+
+public class _11382 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
