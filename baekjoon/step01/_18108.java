a72bff43a2644f0340cea195ac6fe1f9c6d4ebbc
niiion <niion.eooo@gmail.com>
2024-01-02 07:29:44 +0900

    [initial] 18108. 식을 직접 세워서 계산하는 문제

---
+++ baekjoon/step01/_18108.java
@@ -0,0 +1,13 @@
+package baekjoon.stage01;
+
+import java.util.Scanner;
+
+public class _18108 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int year = scanner.nextInt();
+
+        System.out.println(year - 543);
+    }
+}
