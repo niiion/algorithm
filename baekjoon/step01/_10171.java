572f9a30178ef1f674544cdb46ee5d7271a8b2ac
niiion <niion.eooo@gmail.com>
2024-01-02 07:25:24 +0900

    [initial] 10171. \, ' 등의 문자에 주의하며 고양이를 출력하는 문제

---
+++ baekjoon/step01/_10171.java
@@ -0,0 +1,11 @@
+package baekjoon.stage01;
+
+public class _10171 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
