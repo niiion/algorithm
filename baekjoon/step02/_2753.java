8dc27404d2b94017039fd7e2955c8b0eb2881ca9
niiion <niion.eooo@gmail.com>
2024-01-02 07:37:19 +0900

    [initial] 2753. 윤년을 판별하는 문제

---
+++ baekjoon/step02/_2753.java
@@ -0,0 +1,18 @@
+package baekjoon.stage02;
+
+import java.util.Scanner;
+
+public class _2753 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
