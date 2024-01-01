1499a0ad153b35136399ff0c65ce832e641bc59a
niiion <niion.eooo@gmail.com>
2024-01-02 07:27:45 +0900

    [initial] 10926. 입출력을 응용하는 문제??!

---
+++ baekjoon/step01/_10926.java
@@ -0,0 +1,14 @@
+package baekjoon.stage01;
+
+import java.util.Scanner;
+
+public class _10926 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.nextLine();
+
+        System.out.println(s + "??!");
+    }
+}
