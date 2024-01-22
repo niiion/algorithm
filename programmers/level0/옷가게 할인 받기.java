dc9c0ea21723f504a870fd9b5058c982c65dd6eb
niiion <niion.eooo@gmail.com>
2024-01-22 21:41:43 +0900

    [add] 옷가게 할인 받기

--- programmers/level0/옷가게 할인 받기.java
+++ programmers/level0/옷가게 할인 받기.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         
         if( price >= 500000) {
-            answer = (int)(price*0.8);
+            answer = (int)(price * 0.8);
         } else if( price >= 300000){
             answer = (int)(price * 0.9);
         }else if (price >= 100000){
