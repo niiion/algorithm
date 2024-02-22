6deb5f172d0e4c1c99cd07c2eda95405e734d07c
niiion <niion.eooo@gmail.com>
2024-02-22 13:33:47 +0900

    [refactor] 최댓값 만들기 (2)

--- programmers/level0/최댓값 만들기2.java
+++ programmers/level0/최댓값 만들기2.java
@@ -1,15 +1,11 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        
-        for(int i = 0; i < numbers.length; i++) {
-            for(int j = numbers.length-1; j > i; j--) {
-                if(answer < numbers[i] * numbers[j]) {
-                    answer = numbers[i] * numbers[j];
-                }
-            }
-        }
-        
-        return answer;
+        int l = numbers.length;
+
+        Arrays.sort(numbers);
+
+        return Math.max(numbers[0] * numbers[1], numbers[l-2] * numbers[l-1]);
     }
 }
