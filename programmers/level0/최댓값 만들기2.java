e249485150ace6067661d9939b62b83b1e3188a0
niiion <niion.eooo@gmail.com>
2024-02-22 13:25:39 +0900

    [add] 최댓값 만들기 (2)

---
+++ programmers/level0/최댓값 만들기2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = numbers.length-1; j > i; j--) {
+                if(answer < numbers[i] * numbers[j]) {
+                    answer = numbers[i] * numbers[j];
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
