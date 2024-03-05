f30bab34c143e03078d506e4a116f15be53e6b91
niiion <niion.eooo@gmail.com>
2024-03-05 11:14:31 +0900

    [add] A로 B 만들기

---
+++ programmers/level0/A로 B 만들기.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String before, String after) {
+        int answer = 0;
+        
+        char[] arr1 = before.toCharArray();
+        char[] arr2 = after.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        String str1 = new String(arr1);
+        String str2 = new String(arr2);
+
+        answer = str1.equals(str2) ? 1 : 0;
+        
+        return answer;
+    }
+}
