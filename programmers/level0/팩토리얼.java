1cc98a0ee8e60481737158e3d57037967da56221
niiion <niion.eooo@gmail.com>
2024-03-06 17:11:58 +0900

    [add] 팩토리얼

---
+++ programmers/level0/팩토리얼.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int factorial = 1;
+        int i  = 1;
+        
+        while(true) {
+            factorial *= i;
+            if(factorial > n) {
+                answer = i;
+                break;
+            }
+            
+            i++;
+        } 
+        
+        return answer -1;
+    }
+}
