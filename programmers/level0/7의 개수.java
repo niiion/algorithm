0b3bcc885f4a211aac98170cce854607e0cbf007
niiion <niion.eooo@gmail.com>
2024-03-09 18:20:38 +0900

    [add] 7의 개수

---
+++ programmers/level0/7의 개수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        String s = Arrays.toString(array);
+        String[] arr = s.split("");
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i].equals("7")) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
